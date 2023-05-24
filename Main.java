cc4e800393d50f9ff03237f7a96a58d264cf97fb
Rokas Garmasukis <rokas.garmasukis@gmail.com>
2023-05-24 09:45:30 +0200

    Finish 4th stage

--- Main.java
+++ Main.java
@@ -6,19 +6,51 @@ public class Main {
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Input string:");
-        String input = scanner.nextLine();
+        // System.out.println("Input string:");
+        System.out.println("Input encoded string:");
+        // char[] chars = input.toCharArray();
 
-        char[] chars = input.toCharArray();
+        String codedString = scanner.nextLine();
 
-        String binaryString = charsIntoBinaryString(chars);
+        String binaryString = codedStringToBinaryString(codedString);
 
-        String chuckNorrisString = binaryStringIntoChuckNorrisString(binaryString);
+        String textString = binaryStringToTextString(binaryString);
 
-        printResult(chuckNorrisString);
+        printResult(textString);
     }
 
-    private static String binaryStringIntoChuckNorrisString(String binaryString) {
+    private static String binaryStringToTextString(String binaryString) {
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < binaryString.length(); i += 7) {
+            int binary = Integer.parseInt(binaryString.substring(i, i + 7), 2);
+            sb.append((char)binary);
+        }
+
+        return sb.toString();
+    }
+
+    private static String codedStringToBinaryString(String codedString) {
+        String[] codes = codedString.split(" ");
+
+        StringBuilder sb = new StringBuilder();
+
+        String toAppend;
+        for (int i = 0; i < codes.length; i += 2) {
+            if (codes[i].equals("0")) {
+                toAppend = "1";
+            } else {
+                toAppend = "0";
+            }
+            for (int j = 0; j < codes[i + 1].length(); j++) {
+                sb.append(toAppend);
+            }
+        }
+
+        return sb.toString();
+    }
+
+    private static String binaryStringToCodedString(String binaryString) {
         StringBuilder sb = new StringBuilder();
 
         if (binaryString.charAt(0) == '1') {
@@ -48,7 +80,7 @@ public class Main {
         String digits;
         for (char ch : chars) {
             digits = Integer.toBinaryString(ch);
-            binary = String.format("%07d", Integer.parseInt(digits) );
+            binary = String.format("%07d", Integer.parseInt(digits));
             sb.append(binary);
         }
         return sb.toString();
@@ -60,7 +92,6 @@ public class Main {
         System.out.println(output);
 
     }
-
 
 
 }
